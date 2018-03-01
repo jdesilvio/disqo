1fbb6de31ee56a7cc8fd68cf1bb5d379b6e50652
John DeSilvio <john.desilvio@yahoo.com>
2018-03-01 16:38:43 -0500

    Flesh out discussion models

--- discussions/models.py
+++ discussions/models.py
@@ -1,6 +1,25 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title + ' ({})'.format(self.pub_date)
+
+
+@python_2_unicode_compatible
+class Comment(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return ('[Topic {}] '.format(self.topic_id) +
+                self.comment_text +
+                ' ({})'.format(self.pub_date))
+
