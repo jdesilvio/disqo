6a2e6eaf82a51b38473b0a4348fa797467aa26eb
John DeSilvio <john.desilvio@yahoo.com>
2018-03-01 20:08:41 -0500

    Build out index and detail templates

--- discussions/models.py
+++ discussions/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
+MAX_TITLE_LENGTH = 200
+MAX_DESCRIPTION_LENGTH = 2000
+MAX_COMMENT_LENGTH = 500
+
+
 @python_2_unicode_compatible
 class Topic(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=2000)
+    title = models.CharField(max_length=MAX_TITLE_LENGTH)
+    description = models.CharField(max_length=MAX_DESCRIPTION_LENGTH)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -15,11 +20,10 @@ class Topic(models.Model):
 @python_2_unicode_compatible
 class Comment(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    comment_text = models.CharField(max_length=500)
+    comment_text = models.CharField(max_length=MAX_COMMENT_LENGTH)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return ('[Topic {}] '.format(self.topic_id) +
                 self.comment_text +
                 ' ({})'.format(self.pub_date))
-
--- discussions/views.py
+++ discussions/views.py
@@ -1,16 +1,28 @@
-from django.http import HttpResponse
+from django.shortcuts import (
+    get_object_or_404,
+    get_list_or_404,
+    render)
 from .models import Topic, Comment
+from .models import (
+    MAX_TITLE_LENGTH,
+    MAX_DESCRIPTION_LENGTH,
+    MAX_COMMENT_LENGTH)
 
 
 def index(request):
     latest_topics = Topic.objects.order_by('-pub_date')[:5]
-    response = '\n\n'.join(topic.title for topic in latest_topics)
-    return HttpResponse(response)
+    context = {
+        'latest_topics': latest_topics
+    }
+    return render(request, 'discussions/index.html', context)
 
 
 def detail(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, pk=topic_id)
     comments = Comment.objects.filter(topic_id=topic_id)
-    response = topic.title + ' ' + ' '.join(
-        comment.comment_text for comment in comments)
-    return HttpResponse(response)
+    context = {
+        'topic': topic,
+        'comments': comments,
+        'max_comment_length': MAX_COMMENT_LENGTH
+    }
+    return render(request, 'discussions/detail.html', context)
