a1a2eb05242eaf27cefae1fa7757a08d7faff302
John DeSilvio <john.desilvio@yahoo.com>
2018-03-01 19:17:41 -0500

    Views return correct information

--- discussions/views.py
+++ discussions/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from .models import Topic, Comment
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the discusisons index.")
+    latest_topics = Topic.objects.order_by('-pub_date')[:5]
+    response = '\n\n'.join(topic.title for topic in latest_topics)
+    return HttpResponse(response)
+
+
+def detail(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    comments = Comment.objects.filter(topic_id=topic_id)
+    response = topic.title + ' ' + ' '.join(
+        comment.comment_text for comment in comments)
+    return HttpResponse(response)
